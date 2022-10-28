9605e0263d239e331f9e8ee2f2f2d2853473a9ec
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-28 13:24:35 +0300

    Update dog_on_the_green_chair00.cpp

--- source_code/dog_on_the_green_chair00.cpp
+++ source_code/dog_on_the_green_chair00.cpp
@@ -1,4 +1,4 @@
-// dog_on_the_green_chair00.cpp
+// dog_on_the_chair00.cpp
 //
 
 #include <iostream>
@@ -9,15 +9,18 @@ class Dog {
 	string name;
 
 public:
-	Dog(string nm) { name = nm; } // Constructor with parameters
+	// Constructor with parameters
+	Dog(string nm) { name = nm; }
 
 	void time_for_walk() { cout << endl << name << ": " << "\nIt's time for cat-chase!!!\n"; }
 	void bark() { cout << endl << name << ": " << "\nWow wow wow wow\n"; }
 	string get_name() { return name; }
+
 };
 
 void human_dog_interaction(Dog* a) {
-	cout << "\nMe: " << a->get_name() << " is on the green chair!\n";
+	cout << a->get_name() << " is on the green chair!\n";
+
 };
 
 int main() {
@@ -26,10 +29,16 @@ int main() {
 	Dog my_dog2 = Dog("Tolis");
 	Dog my_dog3 = Dog("Ritsa");
 
-	Dog* on_the_green_chair = &my_dog1;
+	Dog* pointer_to_the_green_chair; 
 	
-	human_dog_interaction(on_the_green_chair);
+	pointer_to_the_green_chair = &my_dog1;
+	human_dog_interaction(pointer_to_the_green_chair);
 
+	pointer_to_the_green_chair = &my_dog2;
+	human_dog_interaction(pointer_to_the_green_chair);
+
+	pointer_to_the_green_chair = &my_dog3;
+	human_dog_interaction(pointer_to_the_green_chair);
 
 	return 0;
 }
