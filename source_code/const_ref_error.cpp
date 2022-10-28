b27bc592d5a755fe2cba9c3bb5f2ff82d50417d3
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-28 23:45:35 +0300

    Create const_ref_error.cpp

---
+++ source_code/const_ref_error.cpp
@@ -0,0 +1,35 @@
+// const_ref_error.cpp
+//
+
+#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    string id;
+};
+
+
+// void change_attempt(Student& s){ // it would work!
+void change_attempt(const Student& s){    // error...
+    s.name = "James Bond";
+    s.id = "007";
+}
+
+void print(const Student& s) {    // error...
+    cout << s.name << endl << s.id << endl;
+}
+
+int main() {
+
+    Student s1;
+    s1.name = "Takis";
+    s1.id = "131";
+
+    //change_attempt(s1);
+
+    print(s1);
+
+    return 0;
+}
