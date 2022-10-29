fd64f702737ea12e697d1e418d238f11d23cd147
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-29 22:48:22 +0300

    Update GetKeyState_example.cpp

--- source_code/GetKeyState_example.cpp
+++ source_code/GetKeyState_example.cpp
@@ -2,9 +2,9 @@
 //
 
 #include <iostream>
-#include <chrono>	// for the time pace
+#include <chrono>	 // for the time pace
 #include <thread>	// for the time pace
-#include <windows.h> // for keyboard input (windows)
+#include <windows.h>   // for keyboard input (windows)
 
 using namespace std;
 
@@ -21,8 +21,8 @@ int main()
 		this_thread::sleep_for(200ms);	// for the time pace
 		
 		if (GetKeyState(VK_NUMPAD0) & 0x8000) // key press
-		{									                   // 0x8000 -> key is
-			cout << "\nexit\n";				        // currently pressed
+		{				     // 0x8000 -> key is
+			cout << "\nexit\n";	    // currently pressed
 			exit = 1;
 		}
 	
