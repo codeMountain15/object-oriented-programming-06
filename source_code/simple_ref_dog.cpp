e46be0a7faa670f765d3d160523e04073949c40a
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-29 18:00:36 +0300

    Update simple_ref_dog.cpp

--- source_code/simple_ref_dog.cpp
+++ source_code/simple_ref_dog.cpp
@@ -15,10 +15,10 @@ public:
 int main() {
 
 	Dog my_dog1 = Dog("Mourgos");
-	
+
 	cout << "&my_dog1 = " << (int)&my_dog1 << endl;
-	cout << "sizeof(mydog1) = " << sizeof(my_dog1) << endl;
+	cout << "sizeof(my_dog1) = " << sizeof(my_dog1) << endl;
 	cout << "sizeof(&my_dog1) = " << sizeof(&my_dog1) << endl;
-	
+
 	return 0;
 }
