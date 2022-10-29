9b08154a09539e74f98313f2b9a5f1c5dd237c73
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-29 22:25:10 +0300

    Create sleep_example.cpp

---
+++ source_code/sleep_example.cpp
@@ -0,0 +1,26 @@
+// sleep_example.cpp
+//
+
+#include <iostream>
+#include <chrono>	   // for the time pace
+#include <thread>	  // for the time pace
+
+using namespace std;
+
+
+
+int main()
+{
+	using namespace std::chrono_literals;  // for the time pace
+										                    // it works for windows
+
+	int exit = 1;
+
+	while (exit <= 10) {
+
+		std::this_thread::sleep_for(200ms);	// for the time pace
+		cout << exit++ << endl;
+	
+	}
+
+}
