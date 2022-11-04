241d3d135affc62752b9865b400125c8149c2c43
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-11-04 22:06:49 +0200

    Update random_example.cpp

--- source_code/random_example.cpp
+++ source_code/random_example.cpp
@@ -8,9 +8,12 @@ using namespace std;
 
 int main()
 {
+    // Providing a seed value
+    srand((unsigned)time(NULL));
+
     for (int i = 0; i < 10; i++)
-        cout << "random number " 
-        << i << ": " << rand() 
+        cout << "random number "
+        << i << ": " << rand()
         << endl;
 
     return 0;
